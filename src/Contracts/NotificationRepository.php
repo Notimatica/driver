83a37cab69681ae0fcc1ee3a639e0c4111e3a55f
Maxim Lanin <max@lanin.me>
2016-10-29 17:28:58 +0300

    Update repos contracts

--- src/Contracts/NotificationRepository.php
+++ src/Contracts/NotificationRepository.php
@@ -4,4 +4,19 @@ namespace Notimatica\Driver\Contracts;
 
 interface NotificationRepository
 {
+    /**
+     * Return all notifications.
+     *
+     * @param  int $limit
+     * @param  int $offset
+     * @return Notification[]
+     */
+    public function all($limit = 0, $offset = 0);
+
+    /**
+     * Make notification.
+     *
+     * @return Notification
+     */
+    public function make();
 }
--- src/Contracts/SubscriberRepository.php
+++ src/Contracts/SubscriberRepository.php
@@ -4,4 +4,28 @@ namespace Notimatica\Driver\Contracts;
 
 interface SubscriberRepository
 {
+    /**
+     * Return all subscribers.
+     *
+     * @param  int $limit
+     * @param  int $offset
+     * @return Subscriber[]
+     */
+    public function all($limit = 0, $offset = 0);
+
+    /**
+     * Subscribe to notifications.
+     *
+     * @param  string $provider
+     * @param  array $data
+     * @return Subscriber
+     */
+    public function subscribe($provider, array $data = []);
+
+    /**
+     * Unsubscribe.
+     *
+     * @param  Subscriber $subscriber
+     */
+    public function unsubscribe(Subscriber $subscriber);
 }
