65034b54d72ca3e8d7605843f9dabf0e5b4e3017
Maxim Lanin <max@lanin.me>
2016-10-29 15:55:33 +0300

    Add first model test

--- tests/StatisticsStorages/ModelTest.php
+++ tests/StatisticsStorages/ModelTest.php
@@ -2,9 +2,19 @@
 
 namespace Notimatica\Driver\Tests\Statistics\Storages;
 
+use Notimatica\Driver\StatisticsStorages\Model;
+use Notimatica\Driver\StatisticsStoragesFactory;
 use Notimatica\Driver\Tests\TestCase;
 
 class ModelTest extends TestCase
 {
+    /**
+     * @test
+     */
+    public function it_can_be_made()
+    {
+        $factory = new StatisticsStoragesFactory();
 
+        $this->assertInstanceOf(Model::class, $factory->make(Model::NAME));
+    }
 }
